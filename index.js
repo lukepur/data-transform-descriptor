0a6e7cf33206b86dbd16fac52e3a0d2dcb25cef8
Luke Purton <luke.purton@gmail.com>
2017-05-16 17:29:31 +1000

    Start scaffolding constructor

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const validate = require('validate.js');
+
+validate.validators.isArray = function (value, opts, key, attributes) {
+  return Array.isArray(value) ? null : 'is not an array';
+};
+
 module.exports = function (inputContstraints, outputConstraints, fn, meta = {}) {
+  console.log('validate:', validate({input: []}, inputContstraints));
+  console.log('inputContstraints:', inputContstraints);
   console.log('meta:', meta);
+
+  return {
+    meta () {
+      return JSON.stringify(meta, null, 2);
+    },
+    validateInput (input) {
+      return validate(input, inputContstraints);
+    },
+    run (input) {
+      const validateResults = validate(input, inputContstraints);
+      if (validateResults) return validateResults;
+      return fn(input);
+    }
+  }
 };
--- test/test.js
+++ test/test.js
@@ -1,3 +1,10 @@
 const transformer = require('../index.js');
+const inputContstraints = require('./test-input-constraints.js');
 
-transformer();
+const myTransformer = transformer(inputContstraints(), {}, (input)=>{
+  console.log('running worker on', input);
+  return 200;
+}, {name: 'Worker'});
+
+console.log('calling meta():', myTransformer.meta());
+console.log('running...', myTransformer.run({input: [1]}));
