4e5d2e9f535e8e8ca6e150d8f3bbaffdde1c976c
Luke Purton <luke.purton@gmail.com>
2017-05-17 15:49:01 +1000

    Fix some errors/typos and add some assertions to test

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const validate = require('validate.js');
 require('./validators/validators')(validate);
 
-module.exports = function (inputContstraints, outputConstraints, fn, meta = {}) {
+module.exports = function (inputConstraints, outputConstraints, fn, meta = {}) {
   return {
     meta () {
-      return JSON.stringify(Object.assign({}, meta, {
-        inputContstraints,
-        outputConstraints
-      }), null, 2);
+      return Object.assign(
+        {},
+        meta,
+        {
+          inputConstraints,
+          outputConstraints
+        }
+      );
     },
     validateInput (input) {
       return validate(input, inputContstraints);
     },
     run (input) {
       // validate input
-      const inputValidateResults = validate(input, inputContstraints);
+      const inputValidateResults = validate(input, inputConstraints);
       if (inputValidateResults) return { inputErrors: inputValidateResults };
 
       // run fn
--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
+const assert = require('assert');
 const transformer = require('../index');
-const inputContstraints = require('./test-input-constraints');
-const outputContstraints = require('./test-output-constraints');
+const inputConstraints = require('./test-input-constraints');
+const outputConstraints = require('./test-output-constraints');
 const fn = require('./test-input-fn');
 
-const myTransformer = transformer(inputContstraints(), outputContstraints(), fn, {name: 'Worker'});
+const adder = transformer(inputConstraints(), outputConstraints(), fn, {name: 'Worker'});
 
-console.log('calling meta():', myTransformer.meta());
-console.log('running...', myTransformer.run({numbers: [1, 2, 3]}));
+assert.equal(adder.meta().name, 'Worker');
+assert.notEqual(adder.meta().inputConstraints, undefined);
+assert.notEqual(adder.meta().outputConstraints, undefined);
+assert.equal(adder.run({numbers: [1,2,3]}), 6);
+assert.equal(adder.run({}).inputErrors.numbers[0], 'Numbers is not an array');
+assert.equal(adder.run({numbers: ['1',2,3]}).inputErrors.numbers[0], 'Numbers should only contain members of type number');
+
+console.log('Adder result:', adder.run({numbers: [1,2,3]}))
 
 process.exit();
